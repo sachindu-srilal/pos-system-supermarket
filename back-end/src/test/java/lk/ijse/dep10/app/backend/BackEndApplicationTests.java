efdf678000d24de22e04d964642f3dfe4b66fcdb
Sachindu Srilal <lal95.ac@gmail.com>
2023-08-28 23:18:31 +0530

    Initial commit

---
+++ back-end/src/test/java/lk/ijse/dep10/app/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep10.app.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
