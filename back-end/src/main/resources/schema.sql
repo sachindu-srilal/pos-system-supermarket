8dcb7282860d7df89bbb820d1e78b59cd7f7b612
Sachindu Srilal <lal95.ac@gmail.com>
2023-08-29 12:39:38 +0530

    setup the database connection pool

---
+++ back-end/src/main/resources/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS customer(
+    id INT AUTO_INCREMENT PRIMARY KEY ,
+    name VARCHAR(100) NOT NULL ,
+    address VARCHAR(250) NOT NULL ,
+    contact VARCHAR(20) NOT NULL
+);
+
+ALTER TABLE customer ADD CONSTRAINT uk_contact UNIQUE (contact);
+
+CREATE TABLE IF NOT EXISTS item (
+    code VARCHAR(50) PRIMARY KEY ,
+    description VARCHAR(250) NOT NULL ,
+    qty INT NOT NULL ,
+    unit_price DECIMAL(10,2) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `order` (
+    id INT AUTO_INCREMENT PRIMARY KEY ,
+    `datetime` DATETIME NOT NULL
+);
