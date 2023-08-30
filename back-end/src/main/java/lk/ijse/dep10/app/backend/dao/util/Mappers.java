246f983e661e7601c74d371e6842014fcbaf1334
Sachindu Srilal <lal95.ac@gmail.com>
2023-08-30 18:47:23 +0530

    Implement the Mappers class

--- back-end/src/main/java/lk/ijse/dep10/app/backend/dao/util/Mappers.java
+++ back-end/src/main/java/lk/ijse/dep10/app/backend/dao/util/Mappers.java
@@ -1,4 +1,50 @@
 package lk.ijse.dep10.app.backend.dao.util;
 
-public class Mappers { // incoming result set convert to customer, item, order etc.
+import lk.ijse.dep10.app.backend.entity.*;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+public class Mappers { // using incoming result set  -> create customer, item, order etc.
+    public static final RowMapper<Customer> CUSTOMER_ROW_MAPPER = ((rs, rowNum) -> {
+        int id =rs.getInt("id");
+        String name=rs.getString("name");
+        String address=rs.getString("address");
+        String contact=rs.getString("contact");
+
+        return new Customer(id, name, address,contact);
+    });
+
+    public static final RowMapper<Item> ITEM_ROW_MAPPER=((rs, rowNum) -> {
+        String code=rs.getString("code");
+        String description=rs.getString("description");
+        int qty=rs.getInt("qty");
+        BigDecimal unit_price=rs.getBigDecimal("unit_price");
+
+        return new Item(code,description,qty,unit_price);
+    });
+
+    public static final RowMapper<Order> ORDER_ROW_MAPPER=((rs, rowNum) -> {
+        int id=rs.getInt("id");
+        Timestamp datetime=rs.getTimestamp("datetime");
+
+        return new Order(id, datetime);
+    });
+
+    public static final RowMapper<OrderCustomer> ORDER_CUSTOMER_ROW_MAPPER=((rs, rowNum) -> {
+        int orderId=rs.getInt("orderId");
+        int customerId=rs.getInt("customerId");
+
+        return new OrderCustomer(orderId,customerId);
+    });
+
+    public static final RowMapper<OrderDetail> ORDER_DETAIL_ROW_MAPPER=((rs, rowNum) -> {
+        int orderId=rs.getInt("orderId");
+        String itemCode=rs.getString("itemCode");
+        BigDecimal unit_price=rs.getBigDecimal("unit_price");
+        int qty=rs.getInt("qty");
+
+        return new OrderDetail(orderId,itemCode,unit_price,qty);
+    });
 }
